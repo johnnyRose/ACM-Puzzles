1ee0fd3f8f22f7b5605d19e1fe6652fa65a8f112
John Rosewicz <john.rosewicz@gmail.com>
2015-10-03 19:39:00 -0500

    add reverse rot, rename for consistency

---
+++ 2014/reverse-rot/rot.cpp
@@ -0,0 +1,82 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_.";
+
+int getRotation(string line) {
+	string temp = "";
+	int i = 0;
+	
+	while (line[i] != ' ') {
+		temp += line[i];
+		++i;
+	}
+	
+	return atoi(temp.c_str());
+}
+
+int indexOf(char target) {
+	for (int i = 0; i < chars.length(); ++i) {
+		if (chars[i] == target) {
+			return i;
+		}
+	}
+	
+	return -1;
+}
+
+string reverseString(string original) {
+	string reversed = "";
+	for (int i = original.length() - 1; i >= 0; --i) {
+		reversed += original[i];
+	}
+	
+	return reversed;
+}
+
+string getMessage(string line) {
+	string message = "";
+	int i = 0;
+	
+	while (line[i] != ' ') {
+		++i;
+	}
+	
+	++i;
+	
+	while (i < line.length()) {
+		if (indexOf(line[i]) >= 0) {
+			message += line[i];
+		}
+		
+		++i;
+	}
+	
+	return reverseString(message);
+}
+
+int main() {
+	ifstream file;
+	string line;
+	
+	file.open("rot.in");
+	
+	while (getline(file, line)) {
+		if (line[0] == '0') {
+			break;
+		}
+		
+		int rotation = getRotation(line);
+		string original = getMessage(line);
+		string translated = "";
+		for (int i = 0; i < original.length(); ++i) {
+			translated += chars[(indexOf(original[i]) + rotation) % chars.size()];
+		}
+		
+		cout << translated << "\n";
+	}
+}
